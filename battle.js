0a337ff08c3b6d637c2e3d0cc7d67735c099b100
Alex Abushady <aabushady214@gmail.com>
2019-10-07 14:58:53 -0400

    Update battle.js
    created classes that will be used in program.

--- battle.js
+++ battle.js
@@ -4,5 +4,26 @@
 (function () {
     "use strict";
 
+    function Player(name, weapons) {
+        this.name = name;
+        this.health = 10;
+        this.strength = 2;
+        this.weapons = weapons;
+    }
 
+    function Weapons(name) {
+        this.name = name;
+        this.damage = (Math.random() * 5) + 1;
+    }
+
+    function Enemy() {
+        this.name = "Enemy";
+        this.health = 5;
+        this.strength = 2;
+    }
+
+    function BattleSimulation(players, enemies) {
+        this.players = players[];
+        this.enemies = enemies[];
+    }
 }());
